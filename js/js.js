a8e7403cd28495daa5c4c56fa82d6d8e016336bf
Boff4er <vova@webatmosfera.com>
2015-05-12 17:41:55 +0300

    init code

---
+++ js/js.js
@@ -0,0 +1,20 @@
+$(function(){
+	resizze();
+
+});
+
+$(window).resize(function(){
+	resizze();
+});
+
+$(window).load(function(){
+	$(".spinner-bg").fadeOut();
+	$(".spinner").fadeOut();
+});
+
+function resizze () {
+	var k = 1.33;
+	var height = $(window).height();
+	$(".game").css({"width": (height * k) + "px"});
+	$(".bg").css({"width": (height * k) + "px"});
+}
